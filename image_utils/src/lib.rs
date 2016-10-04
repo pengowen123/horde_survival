abc6bce9dbbaa567ffec43d9b87a6e5bd9dbd437
pengowen123 <pengowen816@gmail.com>
2016-10-04 11:56:39 -0700

    Move texture loading code to separate crate for compile time improvements Add asset loader

--- image_utils/src/lib.rs
+++ image_utils/src/lib.rs
@@ -1,23 +1,21 @@
 extern crate image;
-extern crate gfx;
 extern crate gfx_device_gl;
+extern crate gfx;
 
-use gfx::tex;
+use gfx::{Factory, tex, format};
 use gfx::handle::ShaderResourceView;
-use gfx_device_gl::Factory;
 
 use std::io::Cursor;
 
-pub fn load_texture(factory: &mut Factory, data: &[u8]) -> ShaderResourceView<gfx_device_gl::Resources, [f32; 4]> {
-    let img = image::load(Cursor::new(data), image::PNG).unwrap().to_rgba();
+pub fn load_texture(factory: &mut gfx_device_gl::Factory, data: &[u8]) -> ShaderResourceView<gfx_device_gl::Resources, [f32; 4]> {
+    let img = image::load(Cursor::new(data), image::PNG).expect("Failed to create texture").to_rgba();
     let (w, h) = img.dimensions();
     load_texture_raw(factory, [w, h], &img.into_vec())
 }
 
-pub fn load_texture_raw<R, F>(factory: &mut F, size: [u32; 2], data: &[u8]) -> ShaderResourceView<R, [f32; 4]>
-    where R: gfx::Resources, F: gfx::Factory<R>
-{
+pub fn load_texture_raw(factory: &mut gfx_device_gl::Factory, size: [u32; 2], data: &[u8]) -> ShaderResourceView<gfx_device_gl::Resources, [f32; 4]> {
     let kind = tex::Kind::D2(size[0] as tex::Size, size[1] as tex::Size, tex::AaMode::Single);
-    let (_, view) = factory.create_texture_const_u8::<gfx::format::Rgba8>(kind, &[data]).unwrap();
+    let (_, view) = factory.create_texture_const_u8::<format::Srgba8>(kind, &[data])
+        .expect("Failed to create raw texture");
     view
 }
