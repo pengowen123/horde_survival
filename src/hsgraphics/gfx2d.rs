81379589ab5dafc1198e2d4a42c4ea3caba1e416
Owen Sanchez <pengowen816@gmail.com>
2017-01-12 16:14:49 -0700

    Fix sky color

--- src/hsgraphics/gfx2d.rs
+++ src/hsgraphics/gfx2d.rs
@@ -1,10 +1,12 @@
-pub use hsgraphics::ColorFormat;
+use hsgraphics::ColorFormat;
 
 /// A color
 pub type Color = [f32; 3];
 
 /// The color used when clearing the screen
-pub const CLEAR_COLOR: [f32; 4] = [1.0; 4];
+// TODO: Replace this with [1.0; 4] (white) whena skybox is implemented
+//       For now, this color is the sky
+pub const CLEAR_COLOR: [f32; 4] = [0.0, 0.35, 0.5, 1.0];
 
 /// Types used for gfx
 /// 2d vertices contain a position, a texture coordinate, and a color
